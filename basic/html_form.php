4bfcd5e55e575df20e86ee5800048133a607c624
Albert <purebone00@gmail.com>
2017-05-03 11:30:48 -0700

    Changed from echoing static html, to a more readable php page

--- basic/html_form.php
+++ basic/html_form.php
@@ -1,28 +1,23 @@
-<?php
-function html_form_code() {
-	
-	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
-	echo '<p>';
-	echo 'First Name (required) <br/>';
-	echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-fname"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
-	echo '</p>';
-	echo '<p>';
-	echo 'Last Name (required) <br/>';
-	echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-lname"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
-	echo '</p>';
-	echo '<p>';
-	echo 'Your Email (required) <br/>';
-	echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" />';
-	echo '</p>';
-	echo '<p>';
-	echo 'Subject (required) <br/>';
-	echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" />';
-	echo '</p>';
-	echo '<p>';
-	echo 'Your Message (required) <br/>';
-	echo '<textarea rows="10" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
-	echo '</p>';
-	echo '<p><input type="submit" name="cf-submitted" value="Send"></p>';
-	echo '</form>';
-}
-?>
+<?php function html_form_code() { ?>  <form action="<?php esc_url( $_SERVER['REQUEST_URI'] ) ?>" method="post">
+    <p>
+      First Name (required) <br/>
+      <input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value=" <?php ( isset( $_POST["cf-fname"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) ?>" size="40" />
+    </p>
+    <p>
+      Last Name (required) <br/>
+      <input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="<?php ( isset( $_POST["cf-lname"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) ?>" size="40" />
+    </p>
+    <p>
+      Your Email (required) <br/>
+      <input type="email" name="cf-email" value="<?php ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) ?>" size="40" />
+    </p>
+    <p>
+      Subject (required) <br/>
+      <input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="<?php ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) ?>" size="40" />
+    </p>
+    <p>
+      Your Message (required) <br/>
+      <textarea rows="10" cols="35" name="cf-message"><?php ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) ?></textarea>
+    </p>
+    <p><input type="submit" name="cf-submitted" value="Send"></p>
+  </form>  <?php } ?>
